c345cab80071399c62df5869b472eefc4c336ac7
Grace Liu <graceliu@Graces-Air.home>
2021-11-02 10:25:25 -0400

    set up future present past

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,28 @@
+var descriptionEl= document.querySelector(".description");
+
 function updateHourItems(){
     //assign classes (past/present/future)
-    
-    //read relevant local storage data (about relevant hour slot)
-    
+    // work hour 9 is 9:00-9:59:59
+    //is present if current time isBetween 9 and 9:59
+    // is past if current time isAfter 9:59
+    // is future is current time isBefore 9
+for(var i=9; i<18; i++) {
+    var hourEl=document.getElementById("hour-"+i);
+    var workHourBegin = $("#hour-"+i);
+    workHourBegin =  moment().hour(i).minute(0).second(0);
+    var workHourEnd = moment(workHourBegin).add(3559,'s')
+    if(moment().isBefore(workHourBegin)){
+        $(hourEl).children().addClass("future");
+    } if(moment().isAfter(workHourEnd)){
+    $(hourEl).children().addClass("past");
+    } if(moment().isBetween(workHourBegin,workHourEnd)){
+    $(hourEl).children().addClass("present");
+    }
+}
+    //read relevant local storage data (about relevant hour slot)    
     };
     function handleSave(e){
-        console.log(e.target);
+    cd    //console.log(e.target);
        var hour = ($(e.target).sibling('.description').val());
     
         //tree traversal getting sibling element
