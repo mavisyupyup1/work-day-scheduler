f750984508569f461584d2cdabc96faa3f7f77cc
Grace Liu <graceliu@Graces-Air.home>
2021-11-04 09:40:10 -0400

    add array to hold tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var descriptionEl= document.querySelector(".description");
-
+// create array to hold tasks
+var tasks ={};
 function updateHourItems(){
     //assign classes (past/present/future)
     // work hour 9 is 9:00-9:59:59
@@ -10,54 +11,66 @@ function updateHourItems(){
     var currentDayEl = document.getElementById("currentDay");
     currentDayEl.textContent = todaysDate;
 
-for(var i=9; i<18; i++) {
-    var hourEl=document.getElementById("hour-"+i);
-    var workHourBegin = $("#hour-"+i);
-    workHourBegin =  moment().hour(i).minute(0).second(0);
-    var workHourEnd = moment(workHourBegin).add(3559,'s')
-    if(moment().isBefore(workHourBegin)){
-        $(hourEl).children().addClass("future");
-    } if(moment().isAfter(workHourEnd)){
-    $(hourEl).children().addClass("past");
-    } if(moment().isBetween(workHourBegin,workHourEnd)){
-    $(hourEl).children().addClass("present");
-    }
-};
-    //read relevant local storage data (about relevant hour slot)    
-    var tasks ={};
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    if (!tasks) {
-        tasks = {
-          hour9: [],
-          hour10: [],
-          hour11: [],
-          hour12: [],
-          hour13: [],
-          hour14: [],
-          hour15: [],
-          hour16: [],
-          hour17: [],
-        };
-      }
+    for(var i=9; i<18; i++) {
+        var hourEl=document.getElementById("hour-"+i);
+        var workHourBegin = $("#hour-"+i);
+        workHourBegin =  moment().hour(i).minute(0).second(0);
+        var workHourEnd = moment(workHourBegin).add(3559,'s')
+        if(moment().isBefore(workHourBegin)){
+            $(hourEl).children().addClass("future");
+        } if(moment().isAfter(workHourEnd)){
+        $(hourEl).children().addClass("past");
+        } if(moment().isBetween(workHourBegin,workHourEnd)){
+        $(hourEl).children().addClass("present");
+        }
     };
-    function handleSave(e){
-       //console.log(e.target);      
-       var hour = ($(e.target).sibling('.description').val());
-       console.log(hour);
-        //tree traversal getting sibling element
-        console.log($(e.target).sibling('.description').val());
-        var value =($(e.target).closest('.time-block').attr('id'));
-    //getting closest (or parent not sibling))element attribute
-        console.log($(e.target).closest('.time-block').attr('id'));
-        localStorage.setItem(hour,value);
-    
-    }
-    function main(){
-        updateHourItems();
-    //add click event to timeSlots
-    //document.addEventListener('click',handleSave())
-    $(document).on('click','.save-btn',handleSave);
-    }
-    
-    //start when domcontent loaded event -- cant deal with the DOM
-    $(document).ready(main);
+    //read relevant local storage data (about relevant hour slot)    
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+//     if (!tasks) {
+//         tasks = {
+//           hour9: [],
+//           hour10: [],
+//           hour11: [],
+//           hour12: [],
+//           hour13: [],
+//           hour14: [],
+//           hour15: [],
+//           hour16: [],
+//           hour17: [],
+//         };
+//       }
+//     // loop over object properties
+//     $.each(tasks, function(list, arr) {
+//     //console.log(list, arr);
+//     // then loop over sub-array
+//     arr.forEach(function(task) {
+//     createTask(task.text, task.date, list);
+//     });
+//   });
+};
+function handleSave(event){
+    console.log(event.target);      
+    var value= ($(event.target).siblings('.description').val());
+    console.log(value);
+    //tree traversal getting sibling element
+    var hour =($(event.target).closest('.time-block').attr('id'));
+    console.log(hour);
+//getting closest (or parent not sibling))element attribute
+    console.log($(event.target).closest('.time-block').attr('id'));
+    localStorage.setItem("hour",hour);
+    tasks.push({
+        text: value,
+        hour: hour
+    });
+    console.log(tasks);
+
+}
+function main(){
+    updateHourItems();
+//add click event to timeSlots
+//document.addEventListener('click',handleSave())
+$(document).on('click','.save-btn',handleSave);
+}
+
+//start when domcontent loaded event -- cant deal with the DOM
+$(document).ready(main);
